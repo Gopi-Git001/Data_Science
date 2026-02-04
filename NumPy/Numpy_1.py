19b72ce03d5eee14397eb744b4f4b99cf5a4dcb3
GopiRam <gopi.thungam.001@gmail.com>
2026-02-04 12:33:57 -0600

    update script

--- NumPy/Numpy_1.py
+++ NumPy/Numpy_1.py
@@ -3,11 +3,17 @@ import numpy as np
 my_list = [1,2,3]
 new_list = [1,3,5,7,9]
 
+my_matrix = [[1,2,3],[4,5,6],[7,8,9]]
+
+
 
 arr = np.array(my_list)
 arr_2 = np.array(new_list)
 
+arr_3 = np.array(my_matrix)
+
 print(arr)
 print(arr_2)
+print(arr_3)
 
 
